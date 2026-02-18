88390aa86e79968dd9059c0ca0eef6908dfb9981
EDGAR SOARES DA SILVA <132958606+GENESIS-SOARES17@users.noreply.github.com>
2026-02-18 18:58:47 -0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,19 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { PrivyProvider } from '@privy-io/react-auth';
 import App from './App';
-import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <PrivyProvider
       appId="cmlryv7zk04ox0cjv1hfm2dax"
       config={{
-        // FORÇA a recuperação da carteira vinculada ao ID social
         embeddedWallets: {
+          // 'all-users' força o Privy a buscar uma carteira já existente no banco de dados deles
           createOnLogin: 'all-users', 
           noPromptOnSignature: true,
         },
-        // Garante que a sessão não expire e crie um novo usuário "fantasma"
         loginMethods: ['discord'],
         appearance: {
           theme: 'dark',
